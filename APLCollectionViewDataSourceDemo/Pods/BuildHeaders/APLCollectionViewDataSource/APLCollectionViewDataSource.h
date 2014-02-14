3f9a51c86111122bddc1fe4acb4027062ea00e92
Tobias Conradi <tconradi@apploft.de>
2014-02-14 21:02:33 +0100

    add Pods directory for the demo project

---
+++ APLCollectionViewDataSourceDemo/Pods/BuildHeaders/APLCollectionViewDataSource/APLCollectionViewDataSource.h
@@ -0,0 +1,1 @@
+../../../../Classes/ios/APLCollectionViewDataSource.h
---
+++ APLCollectionViewDataSourceDemo/Pods/Pods-APLCollectionViewDataSourceDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_APLCollectionViewDataSourceDemo : NSObject
+@end
+@implementation PodsDummy_Pods_APLCollectionViewDataSourceDemo
+@end
